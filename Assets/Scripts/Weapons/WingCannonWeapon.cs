4ac1b298ed21a252674b20161b8f917adef66e23
Omar <omar.ahmed.vc@gmail.com>
2019-05-02 12:54:03 +0200

    Add: WingCannon Class and prefab
    Fix : Collisions with Elite units and turret

---
+++ Assets/Scripts/Weapons/WingCannonWeapon.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+// WingCannon Class, it may seems same as MainCannon bec I didnt add much detail yet.
+public class WingCannonWeapon : WEAPON
+{
+    // this base damage for our main cannon.
+    private static int BASEDAMAGE = 200;
+
+    protected override void Start()
+    {
+        // Setting damage value from current WingCannonLevel from ShipDataManager
+        damage = (ShipDataManager.shipDataManager.WingCannonLevel * 10) + BASEDAMAGE;
+        
+        //playing the particle system of the main cannon 
+        GetComponent<ParticleSystem>().Play();
+        
+        // setting power increment , different from weapon to weapon .
+        powerIncrease = 0.1f;
+    }
+
+    // Called when player pick a power up during gameplay
+    public override void PowerUp()
+    {
+        fireRate += powerIncrease;
+        GetComponent<ParticleSystem>().emissionRate = fireRate;
+        Debug.Log("<b><color=Green> Main Cannon Fire Rate = </color></b>");
+    }
+}
